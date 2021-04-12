706bcbdbd7f4df1a761cf4f435375cb890cff162
18339167199 <18339167199@163.com>
2021-04-12 18:13:54 +0800

    首页布局

--- webdev/src/js/home.js
+++ webdev/src/js/home.js
@@ -1,3 +1,20 @@
 var app = require('./lib/app.js');
+app.loadHeader();
 
 require('./lib/header.js');
+
+
+// 
+var home = {
+  adoptMinHeight: function(el) {
+    var minHeight = document.documentElement.clientHeight;
+    
+    el = document.querySelector(el) || document.querySelector('.lm-home');
+    el.style.minHeight = minHeight + 'px';
+  },
+  init: function() {
+    this.adoptMinHeight('.lm-home');
+  }
+}
+
+home.init();
--- webdev/src/js/lib/header.js
+++ webdev/src/js/lib/header.js
@@ -8,17 +8,26 @@
   var collapse_btn = header.querySelector(config.prefix + '-collapse');
   var collapse_menu = header.querySelector(config.prefix + '-menu');
 
-  if (!header) {
+  if (!header || !collapse_btn || !collapse_menu) {
+    console.log('header.js not executed complete')
     return false;
   }
 
   function collapse() {
     var collapse_height = 0;
-    var ul = collapse_menu.querySelector('ul');
-    if (ul) {
+    var ul = this.querySelector('ul');
+    if (ul && !this.classList.contains('lm-menu-open')) {
       collapse_height = ul.offsetHeight;
+      this.style.height = collapse_height + 'px';
+      this.style.transition = 'height .25s ease 0s';
+      this.classList.add('lm-menu-open');
+    } else {
+      this.setAttribute('style', '');
+      this.classList.remove('lm-menu-open');
     }
-  }
+  };
 
-  collapse_btn.addEventListener('click', collapse, false);
+  var handleMenuOpen = collapse.bind(collapse_menu);
+
+  collapse_btn.addEventListener('click', handleMenuOpen, false);
 }())
